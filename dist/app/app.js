73752574ac8f5c698524461cde9454af91728521
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-03-02 23:58:11 +0600

    Add user and course routers with POST endpoints for user creation and course creation

--- dist/app/app.js
+++ dist/app/app.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
+// middleware
 const logger = (req, res, next) => {
     console.log(req.url, req.method, req.hostname);
     next();
@@ -16,6 +17,30 @@ app.use(express_1.default.text());
 // app.get('/', (req: Request, res: Response) => {
 //   res.send('Hello World12!')
 // })
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+// need to define where need to use router
+app.use("/", userRouter);
+// both are work same way
+app.use("/api/v1/courses", courseRouter);
+userRouter.post('/api/v1/users/create-user', (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "User is created successfully",
+        data: user,
+    });
+});
+courseRouter.post('/create-course', (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "User is created successfully",
+        data: course,
+    });
+});
 // req.params
 // Id finding
 // app.get('/:userId/:subId', (req: Request, res: Response) => {
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 
-import { log } from 'console'
+
 import express, { NextFunction, Request, Response } from 'express'
 const app = express()
 const port = 3000
@@ -9,8 +9,8 @@ const port = 3000
 
 // middleware
 const logger = (req: Request, res: Response, next: NextFunction) => {
-console.log(req.url,req.method,req.hostname)
-next()
+  console.log(req.url, req.method, req.hostname)
+  next()
 }
 
 // parser
@@ -21,24 +21,47 @@ app.use(express.text())
 // })
 
 const userRouter = express.Router()
-userRouter.get('/api/v1/users', (req: Request, res: Response) => {
-  const user=req.body
+const courseRouter = express.Router()
+// need to define where need to use router
+app.use("/", userRouter)
+// both are work same way
+app.use("/api/v1/courses", courseRouter)
+userRouter.post('/api/v1/users/create-user', (req: Request, res: Response) => {
+  const user = req.body
   console.log(user);
-}
+  res.json({
+    success: true,
+    message: "User is created successfully",
+    data: user,
+  })
+})
+
+
+courseRouter.post('/create-course', (req: Request, res: Response) => {
+   const course=req.body;
+   console.log(course);
+   
+  res.json({
+   
+    success: true,
+    message: "User is created successfully",
+    data: course,
+  })
+})
 
 // req.params
 // Id finding
 // app.get('/:userId/:subId', (req: Request, res: Response) => {
 //   console.log(req.params);
-  
+
 //   res.send('Hello World12!')
 // })
 
 // req.query
 // email finding
-app.get('/',logger, (req: Request, res: Response) => {
+app.get('/', logger, (req: Request, res: Response) => {
 
-  
+
   res.send('Hello World12!')
 })
 
