19522125bd1e5fd09f1a9976f77ac57bd72f1a85
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-03-02 21:47:29 +0600

    Add logger middleware skeleton to the Express app

--- dist/app/app.js
+++ dist/app/app.js
@@ -6,6 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
+const logger = (req, res, next) => {
+};
 // parser
 app.use(express_1.default.json());
 app.use(express_1.default.text());
